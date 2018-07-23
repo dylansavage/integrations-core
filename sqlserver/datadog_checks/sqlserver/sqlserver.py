c3135e5497044f121d21f80fc6688d9ac26197f3
Christine <cchen7196@gmail.com>
2018-07-23 17:20:22 -0400

    Remove rake stuff

--- sqlserver/datadog_checks/sqlserver/sqlserver.py
+++ sqlserver/datadog_checks/sqlserver/sqlserver.py
@@ -10,6 +10,7 @@ http://blogs.msdn.com/b/psssql/archive/2013/09/23/interpreting-the-counter-value
 import traceback
 from contextlib import contextmanager
 from collections import defaultdict
+
 # 3rd party
 import adodbapi
 from six import iteritems
@@ -18,10 +19,9 @@ try:
 except ImportError:
     pyodbc = None
 
-from config import _is_affirmative
-
 # project
 from datadog_checks.checks import AgentCheck
+from datadog_checks.config import is_affirmative
 
 EVENT_TYPE = SOURCE_TYPE_NAME = 'sql server'
 ALL_INSTANCES = 'ALL'
@@ -42,7 +42,7 @@ COUNTER_TYPE_QUERY = '''select distinct cntr_type
 BASE_NAME_QUERY = '''select distinct {metric_type}
                      from sys.dm_os_performance_counters
                      where ({metric_type}=? or {metric_type}=?
-                     or {metric_type}=?) and cntr_type={};'''.format(PERF_LARGE_RAW_BASE)
+                     or {metric_type}=?) and cntr_type=%s;''' % (PERF_LARGE_RAW_BASE)
 
 INSTANCES_QUERY = '''select instance_name
                      from sys.dm_os_performance_counters
@@ -143,7 +143,7 @@ class SQLServer(AgentCheck):
                             self._make_metric_list_to_collect(instance, self.custom_metrics)
                 else:
                     # How much do we care that the DB doesn't exist?
-                    ignore = _is_affirmative(instance.get("ignore_missing_database", False))
+                    ignore = is_affirmative(instance.get("ignore_missing_database", False))
                     if ignore is not None and ignore:
                         # not much : we expect it. leave checks disabled
                         self.do_check[instance_key] = False
@@ -840,7 +840,9 @@ class SqlOsWaitStat(SqlServerMetric):
 
         placeholder = '?'
         placeholders = ', '.join(placeholder for unused in counters_list)
-        query_base = 'select * from sys.dm_os_wait_stats where wait_type in ({})'.format(placeholders)
+        query_base = '''
+            select * from sys.dm_os_wait_stats where wait_type in ({})
+            '''.format(placeholders)
         cursor.execute(query_base, counters_list)
         rows = cursor.fetchall()
         columns = [i[0] for i in cursor.description]
@@ -920,7 +922,9 @@ class SqlOsMemoryClerksStat(SqlServerMetric):
 
         placeholder = '?'
         placeholders = ', '.join(placeholder for unused in counters_list)
-        query_base = 'select * from sys.dm_os_memory_clerks where type in ({})'.format(placeholders)
+        query_base = '''
+            select * from sys.dm_os_memory_clerks where type in ({})
+            '''.format(placeholders)
         cursor.execute(query_base, counters_list)
         rows = cursor.fetchall()
         columns = [i[0] for i in cursor.description]
